da7241e528e3b19aaa0accec6faa1c09bad0b89e
Vittus Mikiassen <pressure679@users.noreply.github.com>
2016-02-27 21:36:00 +0100

    Update sample.java

--- sample.java
+++ sample.java
@@ -9,35 +9,58 @@ import com.mpatric.mp3agic.NotSupportedException;
 import com.mpatric.mp3agic.InvalidDataException;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class sample {
 	public static void main(String[] args) {
 		String musicDir = System.getenv("HOME");
-		musicDir = musicDir + "\\Music";
+		musicDir = musicDir + "/Music";
+		System.out.println(musicDir);
 		String[] artistsBuffer;
 		String[] albumsBuffer;
 		String[] musicBuffer;
 
 		try {
+			
+			// gets artists
 			artistsBuffer = getDir(new File(musicDir));
+			
+			// iterate artists
 			for (String artistsIterator : artistsBuffer) {
-				musicBuffer = getMusic(new File(musicDir + "\\" + artistsIterator));
+				musicBuffer = getMusic(new File(musicDir + "/" + artistsIterator));
+				System.out.println("-\tartistsIterator: " + artistsIterator);
+				
+				// edit mp3 files from artists
 				if (musicBuffer != null) {
 					for (String musicIterator : musicBuffer) {
-					editMusic(musicDir, artistsIterator, "", musicIterator);
+						editMusic(musicDir, artistsIterator, "", musicIterator);
+						System.out.println("-\t-\tmusicIterator: " + musicIterator);
+						System.out.println("\t-\teditMusic: " + musicDir + " " + artistsIterator + " " + musicIterator);
 					}
-					albumsBuffer = getDir(new File(musicDir + "\\" + artistsIterator));
+
+					// gets albums from artists
+					albumsBuffer = getDir(new File(artistsIterator));
+					System.out.println("\t-\tgetDir: " + artistsIterator);
+
+					// edits music from an album from an arist
 					if (albumsBuffer != null) {
 						for (String albumsIterator : albumsBuffer) {
-							musicBuffer = getMusic(new File(albumsIterator));
+							musicBuffer = getMusic(new File(albumsIterator)); // doesn't get music in Fetty Wap - Deluxe
+							System.out.println("\t-\t-\tgetMusic: " + albumsIterator);
+							for (String s : musicBuffer) {
+								System.out.println("\t-\t-\t" + s);
+							}
 							if (musicBuffer != null) {
 								for (String musicIterator : musicBuffer) {
+									System.out.println("\t-\t-\t-\tmusicIterator: " + musicIterator);
 									editMusic(musicDir, artistsIterator, albumsIterator, musicIterator);
+									System.out.println("\t-\t-\t-\teditMusic: " + musicDir + " " + artistsIterator + " " + musicIterator);
 								}
 							}
 						}
 					}
 				}
+				System.out.println();
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -50,29 +73,48 @@ public class sample {
 		}
 	}
 
-	public static String[] getDir(File dir) {
+	public static String[] getDir(File dir) { // gets directories
 		List<File> albumList = new ArrayList<File>();
+		Iterator<File> albumIterator = null;
 		String[] buffer;
+		int counter = -1;
 		if (dir.listFiles() != null) {
-			for (File albumIterator : dir.listFiles()) {
-				if (!albumIterator.isFile()) {
-					albumList.add(albumIterator);
+			for (File albums : dir.listFiles()) {
+				if (!albums.isFile()) {
+					albumList.add(albums);
 				}
 			}
+			albumIterator = albumList.iterator();
 		}
 		buffer = new String[albumList.size()];
-		albumList.toArray(buffer);
+		if (albumIterator != null) {
+			while (albumIterator.hasNext()) {
+				counter++;
+				buffer[counter] = albumIterator.next().toString();
+			}
+		}
 		return buffer;
 	}
 
-	public static String[] getMusic(File dir) {
+	public static String[] getMusic(File dir) { // gets .mp3 files
 		List<String> musicList = new ArrayList<String>();
+		Iterator<String> musicIterator = null;
 		String[] buffer;
-		if (dir.listFiles() != null) {
-			for (File musicIterator : dir.listFiles()) {
-				if (dir.isFile()) {
-					musicList.add(dir.getName());
-				}
+		int counter = -1;
+		if (dir.list() != null) {
+			for (String music : dir.list()) {
+				//if (dir.isFile()) {
+				System.out.println("\tgetMusic() music: " + music);
+				musicList.add(music);
+				//}
+			}
+			musicIterator = musicList.iterator();
+		}
+		buffer = new String[musicList.size()];
+		if (musicIterator != null) {
+			while (musicIterator.hasNext()) {
+				counter++;
+				buffer[counter] = musicIterator.next();
 			}
 		}
 		buffer = new String[musicList.size()];
@@ -82,30 +124,43 @@ public class sample {
 
 	public static void editMusic(String dir, String artist, String album, String file) throws IOException, UnsupportedTagException, NotSupportedException, InvalidDataException {
 		Mp3File mp3File;
+		ID3v2 id3v2Tag = new ID3v24Tag();
+		if (!file.substring(file.length() - 3, file.length()).equals("mp3")) {
+				return;
+		}
+		artist = artist.split("/")[4];
+		album = album.split("/")[5];
+		System.out.println(dir);
+		System.out.println(artist);
+		System.out.println(album);
+		System.out.println(file);
 		if (album == "") {
-			mp3File = new Mp3File(dir + "\\" + artist + "\\" + file + ".mp3");
+			mp3File = new Mp3File(dir + "/" + artist + "/" + file);
 		} else {
-			mp3File = new Mp3File(dir + "\\" + artist + "\\" + album + "\\" + file + ".mp3");
+			mp3File = new Mp3File(dir + "/" + artist + "/" + album + "/" + file);
 		}
 
 		if (mp3File.hasId3v1Tag()) {
 			mp3File.removeId3v1Tag();
 		}
+		if (mp3File.hasId3v2Tag()) {
+			mp3File.removeId3v2Tag();
+		}
 		if (mp3File.hasCustomTag()) {
 			mp3File.removeCustomTag();
 		}
 
-		ID3v2 id3v2Tag;
-		if (mp3File.hasId3v2Tag()) {
-			id3v2Tag = mp3File.getId3v2Tag();
-		} else {
-			id3v2Tag = new ID3v24Tag();
-			mp3File.setId3v2Tag(id3v2Tag);
-		}
 		id3v2Tag.setArtist(artist);
-		id3v2Tag.setTitle(file);
 		id3v2Tag.setAlbum(album);
+		id3v2Tag.setTitle(file);
 
-		mp3File.save(dir + "\\" + artist + "\\" + "album" + "\\" + file + ".mp3");
+		if (album.equals("")) {
+			mp3File.save(dir + "/" + artist + "/" + file);
+			System.out.println("saved " + dir + "/" + artist + "/" +  file);
+		} else {
+			mp3File.save(dir + "/" + artist + "/" + album + "/" + file);
+			System.out.println("saved " + dir + "/" + artist + "/" + album + "/" +  file);
+		}
+		System.out.println("saved " + file);
 	}
 }
