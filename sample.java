c54fe94a025d0478acb5a29063d7c73b907a76b2
Vittus Mikiassen <pressure679@users.noreply.github.com>
2016-02-29 18:05:11 +0100

    Update sample.java

--- sample.java
+++ sample.java
@@ -10,7 +10,6 @@ import com.mpatric.mp3agic.NotSupportedException;
 import com.mpatric.mp3agic.InvalidDataException;
 import java.util.List;
 import java.util.ArrayList;
-import java.util.Iterator;
 
 public class sample {
 	public static void main(String[] args) {
